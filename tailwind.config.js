64615293ebb744b68c02f60459f31b361cc935b6
Lautaro <acostaquintanalautaro@gmail.com>
2023-07-19 10:28:51 -0300

    nada importante

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,9 @@ module.exports = {
     },
     colors: {
       "dark-green": "#002E00",
-      green: "#1E9F1E",
-      orange: "#E28000",
-      cyan: "#1B8D73",
+      "green": "#1E9F1E",
+      "orange": "#E28000",
+      "cyan": "#1B8D73",
       "slate-blue1": "#546678",
       "slate-blue2": "#91ACB9",
       "white-1": "#fdfbfb",
